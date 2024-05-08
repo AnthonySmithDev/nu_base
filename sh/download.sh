acc5b2dc380995ed463b5f42f31b777aa2e692ab
Anthony Smith <anthonyasdeveloper@gmail.com>
2024-05-08 11:12:15 -0500

    refactor download script

--- sh/download.sh
+++ sh/download.sh
@@ -1,28 +1,31 @@
 
-dir="$HOME/.local/bin"
-file="nu"
-path="$dir/$file"
+version="0.93.0"
+bin="$HOME/.local/bin"
+share="$bin/share"
+path="$share/nu_$version"
 
-mkdir -p $dir
+mkdir -p "$share"
 
 download() {
-  version="0.93.0"
-  wget --quiet --show-progress "https://github.com/nushell/nushell/releases/download/$version/nu-$version-x86_64-unknown-linux-gnu.tar.gz"
-  tar -xf "nu-$version-x86_64-unknown-linux-gnu.tar.gz"
-  rm "nu-$version-x86_64-unknown-linux-gnu.tar.gz"
-  mv "nu-$version-x86_64-unknown-linux-gnu/nu" $path
-  rm -rf "nu-$version-x86_64-unknown-linux-gnu"
+  if [ ! -e "$path" ]; then
+    echo "Starting to download nushell nu_$version"
+
+    wget --quiet --show-progress "https://github.com/nushell/nushell/releases/download/$version/nu-$version-x86_64-unknown-linux-gnu.tar.gz"
+    tar -xf "nu-$version-x86_64-unknown-linux-gnu.tar.gz"
+    rm "nu-$version-x86_64-unknown-linux-gnu.tar.gz"
+    mv "nu-$version-x86_64-unknown-linux-gnu/nu" $path
+    rm -rf "nu-$version-x86_64-unknown-linux-gnu"
+  else
+    echo "Nushell is now downloaded nu_$version"
+  fi
+
+  ln -sf "$path" "$bin/nu"
 }
 
 ping -c 1 google.com >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "You are connected to the internet."
-  if [ ! -f $path ]; then
-    echo "Starting to download nushell"
-    download
-  else
-    echo "Nushell is now downloaded"
-  fi
+  download
 else
   echo "You are not connected to the internet."
 fi
